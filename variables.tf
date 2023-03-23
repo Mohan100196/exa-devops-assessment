b56ef744260f5424277adaceb32e39bbf14c9760
Mohan100196 <113365173+Mohan100196@users.noreply.github.com>
2023-03-24 03:55:56 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "region" {
+  type = string
+  default = "ap-south-1"
+  
+}
+
+variable "subnet_id_1" {
+
+  type = string
+
+  default = "subnet-08d19f331881ffcf1"
+
+ }
+ variable "subnet_id_2" {
+
+  type = string
+
+  default = "subnet-07510bfce2237cd0c"
+
+ }
+ variable "subnet_id_3" {
+  
+  type = string
+
+  default = "subnet-0d71c11baee2692ec"
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "vpc-03f795f2a86bb237b"
+   
+ }
